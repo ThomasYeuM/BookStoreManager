59400759359192edf96f94963a6ef76be213119c
tunang <nguyentuan20042207@gmail.com>
2024-09-19 00:13:23 +0700

    check branch

---
+++ src/test/test.java
@@ -0,0 +1,14 @@
+package test;
+
+import module.Book;
+
+public class test {
+	public static void main(String[] args) {
+		Book book1 = new Book();
+		
+		book1.setName("sfasdf");
+		
+		System.out.println(book1.getName());
+		
+	}
+}
