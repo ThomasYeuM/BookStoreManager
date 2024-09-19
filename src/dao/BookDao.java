7bd8e79ebb3cc4e8482d60c509447e15cc3a1574
tunang <nguyentuan20042207@gmail.com>
2024-09-19 23:36:05 +0700

    Add DAO interfaces

---
+++ src/dao/BookDao.java
@@ -0,0 +1,33 @@
+package dao;
+
+import java.io.IOException;
+import java.util.List;
+
+public class BookDao implements DAO<T> {
+	
+	
+	@Override
+	public List<T> getAll() throws ClassNotFoundException, IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean add(T t) throws ClassNotFoundException, IOException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(T t) throws ClassNotFoundException, IOException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(T t) throws ClassNotFoundException, IOException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/dao/DAO.java
@@ -0,0 +1,14 @@
+package dao;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface DAO<T> {
+	List<T> getAll() throws ClassNotFoundException, IOException;
+	
+	boolean add(T t) throws ClassNotFoundException, IOException;
+	
+	boolean update(T t) throws ClassNotFoundException, IOException;
+	
+	boolean delete(T t) throws ClassNotFoundException, IOException;
+}
