b18579c7c34cadcf1d12fb99432e3c8ca8ef9c07
DauQDung <Dung272400@gmail.com>
2024-09-20 11:05:17 +0700

    bookDao

--- src/dao/BookDao.java
+++ src/dao/BookDao.java
@@ -7,34 +7,6 @@ import java.util.function.Predicate;
 public class BookDao implements DAO<T> {
 	
 	
-	@Override
-	public List<T> getAll() throws ClassNotFoundException, IOException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean add(T t) throws ClassNotFoundException, IOException {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean update(T t) throws ClassNotFoundException, IOException {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean delete(T t) throws ClassNotFoundException, IOException {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public T get(Predicate<T> predicate) throws ClassNotFoundException, IOException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
 
 }
