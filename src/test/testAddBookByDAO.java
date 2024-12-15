5aa57c5b333f4bfc20d9e82472158dc33eb658a0
DauQDung <Dung272400@gmail.com>
2024-12-16 05:44:12 +0700

    categoryViews with add, delete. without cate edit, fix bookview

--- src/test/testAddBookByDAO.java
+++ src/test/testAddBookByDAO.java
@@ -11,29 +11,32 @@ import model.Book;
 public class testAddBookByDAO {
 	public static void main(String[] args) throws ClassNotFoundException, IOException {
 		BookDao bookDao = new BookDao();
-		System.out.println("Test add book");
-		//Add them sach vao file
+//		System.out.println("Test add book");
+//		//Add them sach vao file
 		List<Book> list = new ArrayList<Book>();
-		
-		Book book1 = new Book(10, "test1", 10, 10, "Str", "cai gi do");
-		Book book2 = new Book(30, "test2", 10, 10, "Str", "cai gi do");
-		list.add(book1);
-		
-		list.add(book2);
-		bookDao.add(book2);
-		System.out.println("Add completed");
-//		//Them sach vao file
-//		Book updatedBook = new Book(30, "updatedBook", 10, 10, "STr", "cai gi do");
-//		bookDao.update(updatedBook);
 //		
-//		//Xoa sach khoi file
-//		Book deleteBook = new Book(30, "updatedBook", 10, 10, "Steasd", "cai gi do");
-//		bookDao.delete(deleteBook);
+//		Book book1 = new Book(10, "test1", 10, 10, "Str", "cai gi do");
+//		Book book2 = new Book(30, "test2", 10, 10, "Str", "cai gi do");
+//		list.add(book1);
 //		
-		//Lay danh sach tat ca cac sach da them
-//		List<Book> listBooks = bookDao.getAll();
-//System.out.println("asdkjas");
-			
+//		list.add(book2);
+//		bookDao.add(book2);
+//		System.out.println("Add completed");
+////		//Them sach vao file
+////		Book updatedBook = new Book(30, "updatedBook", 10, 10, "STr", "cai gi do");
+////		bookDao.update(updatedBook);
+////		
+////		//Xoa sach khoi file
+////		Book deleteBook = new Book(30, "updatedBook", 10, 10, "Steasd", "cai gi do");
+////		bookDao.delete(deleteBook);
+////		
+//		//Lay danh sach tat ca cac sach da them
+////		List<Book> listBooks = bookDao.getAll();
+////System.out.println("asdkjas");
+			list = bookDao.getAll();
+			for (Book book : list) {
+				System.out.println(book);
+			}
 		
 		
 	}
