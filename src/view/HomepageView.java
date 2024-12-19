8d947ec184ace2df3d36fc3dfd8039ffcc3320ca
DauQDung <Dung272400@gmail.com>
2024-12-19 08:12:50 +0700

    1 cai gi do

--- src/view/HomepageView.java
+++ src/view/HomepageView.java
@@ -1,15 +1,25 @@
 package view;
 
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Map;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
-import java.awt.Font;
-import java.awt.event.ActionListener;
-import java.io.IOException;
-import java.awt.event.ActionEvent;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+import dao.BillDAO;
+import model.Bill;
+
 
 public class HomepageView extends JFrame {
 
@@ -89,6 +99,45 @@ public class HomepageView extends JFrame {
 		chartsbtn.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		chartsbtn.setBounds(125, 299, 221, 50);
 		contentPane.add(chartsbtn);
+		chartsbtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+				Map<String, Double> revenue = new TreeMap<String, Double>();
+
+				BillDAO billDAO = new BillDAO();
+				Bill billController = new BillController(billDAO, null);
+				try {
+					List<Bill> bills = billController.getAllBills();
+
+					for (int i = 1; i <= 12; i++) {
+						String month = String.format("%02d", i);
+						revenue.put(month, revenue.getOrDefault(month, 0.0));
+					}
+
+					for (Bill bill : bills) {
+						String month = bill.getDate().substring(3, 5);
+						revenue.put(month, revenue.getOrDefault(month, 0.0) + bill.getTotal());
+					}
+
+					for (Map.Entry<String, Double> entry : revenue.entrySet()) {
+						dataset.addValue(entry.getValue(), "Doanh thu (đồng)", entry.getKey());
+					}
+
+					JFreeChart barChart = ChartFactory.createBarChart("Thống kê doanh thu", "Tháng", "Doanh thu (đồng)",
+							dataset, PlotOrientation.VERTICAL, true, true, false);
+
+					JFrame frame = new JFrame();
+					frame.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+					frame.setSize(960, 540);
+					frame.getContentPane().add(new ChartPanel(barChart));
+					frame.setLocationRelativeTo(null);
+					frame.setVisible(true);
+				} catch (ClassNotFoundException | IOException e1) {
+					e1.printStackTrace();
+				}
+
+			}
+		});
 		
 		JButton qlyCategoryBtn = new JButton("Quản lý thể loại");
 		qlyCategoryBtn.addActionListener(new ActionListener() {
